20d1a0492d0d3702b3e2d5bfcc6dadb979f7a471
Zion <zion@drcorp.co.kr>
2023-10-11 17:59:03 +0900

    feat: 섹션 1-2

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users/';
 
 // dom
 var username = document.querySelector('#username');
@@ -13,9 +13,14 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerHTML = user[0].email;
+      // 그냥 문자열이 아니라 객체였음(키:밸류 값)
+      address.innerHTML = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
