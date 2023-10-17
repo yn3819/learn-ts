82f0514437c46495d10064b2288ade5484a94ef8
Zion <zion@drcorp.co.kr>
2023-10-17 09:59:38 +0900

    섹션4-실습풀이5 중복된 코드 제거 및 인터페이스 소개

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,9 +1,33 @@
-//할 일의 목록을 받는 배열(맨 하단)
-// let todoItems: object[]; //ex) : number[], :string[]
-let todoItems : {id:number; title: string; done: boolean}[];
+// 타입 정의하는 법, 근데 이것보단 인터페이스로 정의하기
+// type Todo = {
+//   id: number; title: string; done: boolean;
+// }
 
-// api
-function fetchTodoItems():{id: number; title: string; done: boolean}[] {
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+//할 일의 목록을 받는 배열(맨 하단)
+// 1-1. let todoItems: object[]; //ex) : number[], :string[]
+// 1-2. let todoItems: { id: number; title: string; done: boolean }[];
+
+// 1-3. 인터페이스로 정리
+let todoItems: Todo[];
+
+// 1-1. api
+// function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
+//   const todos = [
+//     { id: 1, title: "안녕", done: false },
+//     { id: 2, title: "타입", done: false },
+//     { id: 3, title: "스크립트", done: false },
+//   ];
+//   return todos; //배열을 반환
+// }
+
+// 1-2. 인터페이스로 정리
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: "안녕", done: false },
     { id: 2, title: "타입", done: false },
@@ -13,12 +37,12 @@ function fetchTodoItems():{id: number; title: string; done: boolean}[] {
 }
 
 // crud methods
-function fetchTodos(): {id: number; title: string; done: boolean}[] {
+function fetchTodos(): Todo[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo: {id: number; title: string; done: boolean}): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -26,7 +50,10 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: {id: number; title: string; done: boolean}): void {
+function completeTodo(
+  index: number,
+  todo: Todo
+): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -47,7 +74,8 @@ function showCompleted(): object[] {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems(): void { // 왜 void일까?
+function addTwoTodoItems(): void {
+  // 왜 void일까?
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
   const item1 = {
     id: 4,
@@ -58,7 +86,7 @@ function addTwoTodoItems(): void { // 왜 void일까?
   addTodo(item1);
   addTodo({
     id: 5,
-    title: 'item5',
+    title: "item5",
     done: false,
   });
 }
