e567813ec1325f256a19f266f7b1cc9dc76b4757
sy2681 <yn3819@naver.com>
2023-10-23 17:15:39 +0900

    feat: 유니온과 인터섹션

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -28,7 +28,7 @@ function logMessage3(value: string | number) {
   throw new TypeError("value must be string or number");
 }
 
-// 2. 유니온 타입의 특징
+// 2-1. 유니온 타입의 특징=============================
 
 interface Developer {
   name: string;
@@ -40,8 +40,30 @@ interface Person {
   age: number;
 }
 
-function askSomeone(someone: Developer | Person) {
-  // someone. //접근이 name만 됨. 공통속성만 됨
-  // someone.skill // 접근안됨
-  // someone.age // 접근안됨. 이것들을 하려면 21줄처럼 if typeof 해야할 것
+// 2-2.
+// function askSomeone(someone: Developer | Person) {
+//    someone. //접근이 name만 됨. 공통속성만 됨
+//    someone.skill // 접근안됨
+//    someone.age // 접근안됨. 이것들을 하려면 20줄처럼 if typeof 해야할 것
+// }
+
+
+// 3-1. 인터섹션 타입 소개
+// var capt: string & number & boolean;
+
+// 3-2. 2-2와 달리 오류 사라짐
+function askSomeone(someone: Developer & Person) {
+  someone.name
+  someone.skill; 
+  someone.age;
 }
+
+// 4-1. 유니온 타입과 인터섹션 타입의 차이점
+// 4-2. 유니온 타입일 때 함수 호출 (디벨로퍼를 주거나 or 펄슨을 주거나)
+// askSomeone({name: '디벨로퍼', skill: '웹 개발'})
+// askSomeone({name: '캡틴', age: 100})
+
+// 4-3. 인터섹션 타입일 때 함수 호출
+// 유니온: 선택지 생김, 인터섹션: 합집합..
+askSomeone({name: '디벨로퍼', skill: '웹 개발', age: 200}) // &인터섹션 씀으로 인해, 개발자속성, 사람의 속성도 넣어주어야 함
+askSomeone({name: '캡틴', age: 100, skill: '스킬'})
